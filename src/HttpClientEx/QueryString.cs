92101766fa0891558ae1c113a8a9b501bb4a1eab
pinopino <myhinata@126.com>
2020-02-27 17:28:57 +0800

    QueryString添加构造函数方便使用

--- src/HttpClientEx/QueryString.cs
+++ src/HttpClientEx/QueryString.cs
@@ -1,3 +1,5 @@
+using FastMember;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,6 +9,22 @@ namespace HttpClientEx
 {
     public class QueryString : Collection<KeyValuePair<string, string>>
     {
+        public QueryString(Dictionary<string, object> dict)
+        {
+            if (dict == null)
+                throw new ArgumentNullException(nameof(dict));
+
+            foreach (var key in dict.Keys)
+                Add(key, dict[key].ToString()); // 所以最好这个object是实现了ToString()的
+        }
+
+        public QueryString(object obj)
+        {
+            var acc = ObjectAccessor.Create(obj);
+            foreach (var member in acc.TypeAccessor.GetMembers())
+                Add(member.Name, acc[member.Name].ToString());
+        }
+
         public void Add(string key, string value)
         {
             Add(new KeyValuePair<string, string>(key, value));
